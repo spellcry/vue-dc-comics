e90a6d1b51b55d1e1e9bf1c6925d24a6f0746d29
spellcry <sangiothebest@hotmail.com>
2022-09-05 16:05:41 +0200

    add main-header component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="app">
-
+    <MainHeader/>
   </div>
 </template>
 
 <script>
+import MainHeader from './components/MainHeader.vue'
 
 export default {
   name: 'App',
   components: {
-
+    MainHeader,
   }
 }
 </script>
 
 <style lang="scss">
-
+  @import './style/index.scss';
 </style>

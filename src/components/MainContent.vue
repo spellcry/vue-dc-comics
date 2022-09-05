d8f646088437e0c492e631abb42f275f6ebf57ad
spellcry <sangiothebest@hotmail.com>
2022-09-05 16:15:31 +0200

    add main-content component

---
+++ src/components/MainContent.vue
@@ -0,0 +1,24 @@
+<template>
+    <main class="main-content">
+        <div class="container">
+            --&gt; Content goes here &lt;--
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+    @import '../style/variables';
+    
+    .main-content {
+        background-color: $color-dark;
+        color: $color-white;
+        padding-block: 3rem;
+    }
+
+</style>

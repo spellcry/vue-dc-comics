2161c260b20557ba93ca2ceef98ea73d5ef91a09
spellcry <sangiothebest@hotmail.com>
2022-09-06 18:36:39 +0200

    refactor sidebar

--- bonus/src/components/MainSidebar.vue
+++ bonus/src/components/MainSidebar.vue
@@ -2,45 +2,55 @@
     <aside class="main-sidebar">
         <div class="container">
             <ul class="aside__list">
-                <li class="list__item">
-                    <a href="#">
-                        <img src="../assets/buy-comics-digital-comics.png" alt="">
-                        Digital Comics
-                    </a>
-                </li>
-                <li class="list__item">
-                    <a href="#">
-                        <img src="../assets/buy-comics-merchandise.png" alt="">
-                        Dc Merchandise
-                    </a>
-                </li>
-                <li class="list__item">
-                    <a href="#">
-                        <img src="../assets/buy-comics-subscriptions.png" alt="">
-                        Subscription
-                    </a>
-                </li>
-                <li class="list__item">
-                    <a href="#">
-                        <img src="../assets/buy-comics-shop-locator.png" alt="">
-                        Comic Shop Locator
-                    </a>
-                </li>
-                <li class="list__item">
-                    <a href="#">
-                        <img src="../assets/buy-dc-power-visa.svg" alt="">
-                        Dc Power Visa
-                    </a>
-                </li>
+                <li v-for="(widget, index) in sidebarWidget" :key="index" class="list__item">
+                    <SidebarWidget :imageSrc="widget.imageSrc" :href="widget.href" :text="widget.text"/>               
+                </li>                
             </ul>
         </div>
     </aside>
 </template>
+
 <script>
-export default {
-    
-}
+    import SidebarWidget from './SidebarWidget.vue';
+
+    export default {
+        data() {
+            return {
+                sidebarWidget: [
+                    {
+                        imageSrc: require('../assets/buy-comics-digital-comics.png'),
+                        text: 'Digital Comics',
+                        href: '#'
+                    },
+                    {
+                        imageSrc: require('../assets/buy-comics-merchandise.png'),
+                        text: 'Dc Merchandise',
+                        href: '#'
+                    },
+                    {
+                        imageSrc: require('../assets/buy-comics-subscriptions.png'),
+                        text: 'Subscription',
+                        href: '#'
+                    },
+                    {
+                        imageSrc: require('../assets/buy-comics-shop-locator.png'),
+                        text: 'Comic Shop Locator',
+                        href: '#'
+                    },
+                    {
+                        imageSrc: require('../assets/buy-dc-power-visa.svg'),
+                        text: 'Dc Power Visa',
+                        href: '#'
+                    },
+                ]
+            }
+        },
+        components: {
+            SidebarWidget
+        }
+    }
 </script>
+
 <style lang="scss" scoped>
     @import '../style/variables';
     @import '../style/mixins';
@@ -53,21 +63,7 @@ export default {
             display: flex;
             justify-content: space-around;
             .list__item {
-                flex-basis: 15%;
-                a {
-                    display: flex;
-                    align-items: center;       
-                    height: 100%;    
-                    white-space: nowrap;    
-                    color: $color-white;
-                    text-transform: uppercase;
-                    font-size: 0.85rem;
-                    img {
-                        max-width: 50px;
-                        max-height: 50px;
-                        margin-right: 1rem;
-                    }
-                }
+                flex-basis: 15%;                                
             }
         }
     }
---
+++ bonus/src/components/SidebarWidget.vue
@@ -0,0 +1,34 @@
+<template>
+    <a :href="href">
+        <img :src="imageSrc" alt="">
+        {{ text }}
+    </a> 
+</template>
+
+<script>
+    export default {
+        props: {
+            imageSrc: String,
+            href: String,
+            text: String
+        }    
+    }
+</script>
+<style lang="scss" scoped>
+    @import '../style/variables';
+
+    a {
+        display: flex;
+        align-items: center;       
+        height: 100%;    
+        white-space: nowrap;    
+        color: $color-white;
+        text-transform: uppercase;
+        font-size: 0.85rem;
+        img {
+            max-width: 50px;
+            max-height: 50px;
+            margin-right: 1rem;
+        }
+    }
+</style>
